db0bade2f47cfe3c35ed3d6e8b011a51b5a1573d
Bahadir Alp Barlas <114253782+balpbarlas@users.noreply.github.com>
2025-05-04 12:03:17 -0700

    Update hexSampling.m

--- functions/functions_added/hexSampling.m
+++ functions/functions_added/hexSampling.m
@@ -1,5 +1,15 @@
 %% Hexagonal Sampling
 function kdata_hexSampled = hexSampling(kdata_all_org)
+%   Interpolation step for 3D GRAPPA Reconstruction
+%   Input:
+%       - kdata: kdata_all_org (Nx x Ny x Nz x Nslice x Ncoil)
+%
+%   Output:
+%       - kdata (hexagonally undersampled): kdata_hexSampled (Nx x Ny x Nz x Nslice x Ncoil)
+%
+%   Author: Bahadir Alp Barlas
+%   Email: bbarlas@usc.edu
+
 [Nx, Ny, Nz, Nslice, Ncoil] = size(kdata_all_org);
 kdata_hexSampled = zeros(Nx,Ny,Nz,Nslice,Ncoil);
 for q=1:Ncoil
